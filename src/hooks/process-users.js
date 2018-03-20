9ef888ebe38aaf7c8261b66cec4fc2a0b0e6222e
askariya <arunaluk@yahoo.ca>
2018-03-20 01:23:54 -0600

    Adding logic for dealing with duplicate users in account creation

--- src/hooks/process-users.js
+++ src/hooks/process-users.js
@@ -26,6 +26,25 @@ module.exports = function (options = {}) {
       throw new Error("Invalid email");
     }
 
+    // context.app.service('users').find({
+    //   query: { username: data.username }
+    // }).then(console.log("hey"))
+
+    const userService = context.app.service('users')
+    const usernames = await userService.find({
+      query: {
+        username: data.username
+      }
+    })
+
+    const emails = await userService.find({
+      query: {
+        email: data.email
+      }
+    })
+
+    confirm_unique_user(emails, usernames)
+
     return context;
   };
 };
@@ -35,9 +54,28 @@ function username_is_valid(username) {
   return /^[0-9a-zA-Z_.-]+$/.test(username);
 }
 
-//regex code adapted from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// regex code adapted from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function email_is_valid(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+/* function to confirm that a given username and email are unique
+if not will send appropriate Errors to the client */
+function confirm_unique_user(emails, usernames){
+  var username_taken = usernames.data.length != 0
+  var email_taken = emails.data.length != 0
+  
+  //check if the user already exists
+  if(username_taken && email_taken){
+    throw new Error("Email and Username are already in use")
+  }
+  else if(email_taken && !username_taken){
+    throw new Error("Email is already in use")
+  }
+  else if(username_taken && !email_taken){
+    throw new Error("Username is already in use")
+  }
+
 }
 
