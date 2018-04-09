2c7a3aff6035e217a9c51ec517908d33b6e1ad19
askariya <arunaluk@yahoo.ca>
2018-04-08 21:08:42 -0600

    Adding completed functions for updating username and password

--- src/hooks/process-user-update.js
+++ src/hooks/process-user-update.js
@@ -7,36 +7,61 @@ var utils = require('./user-utils');
 module.exports = function (options = {}) {
   return async context => {
     const { data } = context; 
+    const { params } = context;
 
     // Throw error if none of the possible fields are filled out
     if (!data.username && !data.password && !data.email) {
       throw new Error('An update request must contain a username, email or password');
     }
-    console.log(context.data);
-    
-    // if a username has been changed
+    // console.log("context")
+    // console.log(context)
+    // adjust query to perform patch for only this user
+    params.query = {
+      username: params.user.username,
+      email: params.user.email
+    }
+
+    const userService = context.app.service('users')
+
+    // if this is a Username change request
     if (data.username && !data.password && !data.email) {
-      const userService = context.app.service('users')
+      // check username is valid
+      if (!utils.username_is_valid(data.username)) {
+        throw new Error("Invalid username");
+      }
+      // get list of existing users with proposed username
       const usernames = await userService.find({
         query: {
           username: data.username
         }
       })
-
+      // check proposed username is unique
+      if(utils.confirm_unique_user(null, usernames)){
+        // if so, add an updated time
+        data.updatedAt = new Date().getTime();
+      }
     }
 
-    // if an email has been changed
+    // if this is an Email change request
     else if (data.email && !data.username && !data.password) {
+      // check email is valid
+      if (!utils.email_is_valid(data.email)) {
+        throw new Error("Invalid email");
+      }
       const emails = await userService.find({
         query: {
           email: data.email
         }
       })
+      if(utils.confirm_unique_user(emails, null)){
+        data.updatedAt = new Date().getTime();
+      }
     }
 
-    data.updatedAt = new Date();
-
-    const user = context.params.user;
+    // if this is a Password change request
+    else if (data.password && !data.email && !data.username) {
+      data.updatedAt = new Date().getTime();
+    }
 
     return context;
   };
--- src/hooks/user-utils.js
+++ src/hooks/user-utils.js
@@ -13,22 +13,30 @@ module.exports = {
     /* function to confirm that a given username and email are unique
     if not will send appropriate Errors to the client */
     confirm_unique_user: function(emails, usernames){
-        var username_taken = usernames.data.length != 0
-        var email_taken = emails.data.length != 0
+
+        if (emails === null) {
+            var email_taken = false;
+            var username_taken = usernames.data.length != 0;
+        }
+
+        else if (usernames === null) {
+            var username_taken = false;
+            var email_taken = emails.data.length != 0;
+        }
 
         //check if the user already exists
-        if(username_taken && email_taken){
-            throw new Error("Email and Username are already in use")
+        if (username_taken && email_taken) {
+            throw new Error("Email and Username are already in use");
         }
-        else if(email_taken && !username_taken){
-            throw new Error("Email is already in use")
+        else if (email_taken && !username_taken) {
+            throw new Error("Email is already in use");
         }
-        else if(username_taken && !email_taken){
-            throw new Error("Username is already in use")
+        else if (username_taken && !email_taken) {
+            throw new Error("Username is already in use");
         }
         // if the user is indeed unique, return true
-        else{
-            return true
+        else {
+            return true;
         }
     }
 };
